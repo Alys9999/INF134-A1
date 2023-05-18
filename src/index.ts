e7a0c5f15304a1189cdf773d9420db5b97d0546b
Alyssa <41v1rj@gmail.com>
2023-05-17 19:55:28 -0700

    novel widget up

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { CRadio } from "./widgets/cradio";
 import { CScrollbar } from "./widgets/CScrollbar";
 import { CScrollthumb } from "./widgets/CScrollthumb";
 import { Ctext } from "./widgets/Ctext";
+import { CNovel } from "./widgets/novel";
 
 
 let w = new Window(window.innerHeight-10,'100%');
@@ -20,73 +21,19 @@ lbl1.fontSize = 16;
 lbl1.move(100,50);
 
 
-let btn2 = new CButton(w);
+let btn2 = new CNovel(w);
 btn2.tabindex = 2;
-btn2.fontSize = 12;
-btn2.setInput="Setted Label";
 btn2.move(100, 100);
+btn2.nextfunction((event:any)=>{
+    lbl1.text="task one triggered";
+}, "blahblah");
 
-btn2.onClick((event:any)=>{
-    lbl1.text="Fire";
-})
+btn2.nextfunction((event:any)=>{
+    lbl1.text="task two triggered";
+}, "hello world");
 
-let lbl2= new Heading(w);
-lbl2.text = "CheckBox Demo";
-lbl2.tabindex = 3;
-lbl2.fontSize = 16;
-lbl2.move(300,50);
+btn2.nextfunction((event:any)=>{
+    lbl1.text="task three triggered";
+}, "yahoooooo!");
+btn2.onClick();
 
-let checkB = new CheckBox(w);
-checkB.tabindex=4;
-checkB.move(300,100);
-checkB.onClick((event:any)=>{
-    if (checkB.getIsPressed){
-        lbl2.text="checked";
-    }else{
-        lbl2.text="Unchecked";
-    }
-})
-
-let lbl3= new Heading(w);
-lbl3.text = "Radio Demo";
-lbl3.tabindex = 5;
-lbl3.fontSize = 16;
-lbl3.move(500,50);
-
-let crad = new CRadio(w,new Array("1","2","3"));
-crad.tabindex=6;
-crad.move(500,100);
-crad.setname(0, "alyssa");
-crad.onClick((event:any)=>{
-    lbl3.text="the item of index "+crad.getPressednum +" is clicked";
-})
-
-let bartext = new Heading(w);
-bartext.tabindex=10;
-bartext.move(700,50);
-bartext.text="bar thumb position";
-
-
-//the click of scrollthumb does not trigger the onCLick of scollbar
-let scrollbar = new CScrollbar(w);
-scrollbar.tabindex=7;
-scrollbar.move(700,100);
-scrollbar.onClick((event:any)=>{
-    bartext.text=String(scrollbar.getthumbpos);
-})
-scrollbar.onMove((event:any)=>{
-    bartext.text=String(scrollbar.getthumbpos);
-})
-
-
-let lbl4 = new Heading(w);
-lbl4.text = "Ctext";
-lbl4.tabindex = 8;
-lbl4.move(900, 500);
-
-let ctext = new Ctext(w);
-ctext.tabindex = 9;
-ctext.move(900,100);
-ctext.onClick((event:any)=>{
-    lbl4.text="typing: " +ctext.getInput;
-});
---
+++ src/widgets/novel.ts
@@ -0,0 +1,165 @@
+// importing local code, code we have written
+import {EventArgs, IdleUpWidgetState, PressedWidgetState } from "../core/ui";
+import {Window, Widget, RoleType} from "../core/ui";
+import {Rect, Text, Box} from "../core/ui";
+// importing code from SVG.js library
+import {Circle} from "../core/ui";
+
+
+
+class CNovel extends Widget{
+    private _circle: Circle;
+    private _text: Text;
+    private _fontSize: number;
+    private _inputArray: string[] = [];
+    private _input: string;
+    private _text_y: number;
+    private _text_x: number;
+    private _radius: number;
+    private _taskArray: {(event?:any):void}[] = [];
+    private defaultText: string= "Circle Button";
+    private defaultFontSize: number = 12;
+    private defaultRadius: number = 40
+
+
+    constructor(parent:Window){
+        super(parent);
+        // set defaults
+        this._input = this.defaultText;
+        this._fontSize = this.defaultFontSize;
+        this._radius = this.defaultRadius;
+        // set Aria role
+        this.role = RoleType.button;
+        //TODO:
+        // set default state!
+        this.setState(new IdleUpWidgetState());
+        // render widget
+        this.render();
+    }
+
+    render(): void {
+        this._group = (this.parent as Window).window.group();
+        this._circle = this._group.circle(this._radius);
+        this._text = this._group.text(this._input);
+        this._circle.stroke("black");
+        this._circle.fill("#00FFCA");
+        // Set the outer svg element 
+        this.outerSvg = this._group;
+        // Add a transparent rect on top of text to prevent selection cursor
+        this._group.circle(this._radius).opacity(0).attr('id', 0);
+        // register objects that should receive event notifications.
+        // for this widget, we want to know when the group or rect objects
+        // receive events
+        this.registerEvent(this.outerSvg);
+    }
+
+    private positionText(){
+        let box:Box = this._text.bbox();
+        // in TS, the prepending with + performs a type conversion from string to number
+        this._text_y = (+this._circle.y() + ((+this._circle.attr("r"))) - (box.height/2));
+        this._text_x = (+this._circle.x() + ((+this._circle.attr("r"))) - (box.width/2));
+        if (this._text_y > 0){
+            this._text.y(this._text_y);
+        }
+        if (this._text_x > 0){
+            this._text.x(this._text_x)
+        }
+    }
+
+    nextfunction(func: {(event?:any):void}, text: string){
+        console.log(func);
+        console.log(text);
+        console.log(this._inputArray);
+        this._inputArray.push(text);
+        this._taskArray.push(func);
+    }
+
+    override update(): void {
+        if(this._text != null)
+            this._text.font('size', this._fontSize);
+            this._text.text(this._input);
+            this.positionText()
+
+        if(this._circle != null)
+            this._circle.fill(this.backcolor);
+        if(this._radius != null)
+            this._circle.radius(this._radius);
+        
+        super.update();
+    }
+
+    set fontSize(size:number){
+        this._fontSize= size;
+        this.update();
+    }
+
+    set setInput(text:string){
+        this._input = text;
+        this.update();
+    }
+
+    get getInput(){
+        return this._input;
+    }
+
+    set setRadius(r:number){
+        this._radius = r;
+        this.update();
+    }
+
+    get getRadiu(){
+        return this._radius;
+    }
+
+    onClick():void{
+    }
+
+
+    //TODO: give the states something to do! Use these methods to control the visual appearance of your
+    //widget
+    idleupState(): void {
+        this.backcolor="#00FFCA";
+    }
+    idledownState(): void {
+    }
+    pressedState(): void {
+        this.backcolor="#088395";
+    }
+    pressReleaseState(): void {
+        this.backcolor="#00FFCA";
+        let input = this._inputArray.shift();
+        this.setInput = input;
+        this._inputArray.push(input);
+        let task = this._taskArray.shift();
+        this._taskArray.push(task);
+       this.attach(task, new PressedWidgetState());
+        this.raise(new EventArgs(this), new PressedWidgetState());
+    }
+    hoverState(): void {
+        this.backcolor="#05BFDB";
+    }
+    hoverPressedState(): void {
+        this.backcolor="#088395";
+    }
+    pressedoutState(): void {
+        this.backcolor="#00FFCA";
+    }
+    moveState(): void {
+        
+    }
+    keyupState(keyEvent?: KeyboardEvent): void {
+        if (keyEvent.key == "Backspace"){
+            let input = this._inputArray.pop();
+            console.log(input);
+            this.setInput = input;
+            this._inputArray.unshift(input);
+            let task = this._taskArray.pop();
+            this._taskArray.unshift(task);
+           this.attach(task, new PressedWidgetState());
+            this.raise(new EventArgs(this), new PressedWidgetState());
+        }
+        
+    }
+}
+
+export {CNovel}
